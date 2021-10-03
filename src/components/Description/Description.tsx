c15c0003db2d0d994d0dd3c95976489119dea63c
Solar <gd.gvidas@gmail.com>
2021-10-03 21:46:31 +0300

    Changed developer to engineer

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -5,7 +5,7 @@ const Description: FunctionComponent = () => {
 	return (
 		<section className={styles.container}>
 			<span className={styles.name}>Gvidas Gaidauskas</span>
-			<span className={styles.title}>Software developer</span>
+			<span className={styles.title}>Software engineer</span>
 			<section className={styles.contacts}>
 				<a href="mailto:gd.gvidas@gmail.com" className={styles.email}>gd.gvidas@gmail.com</a>
 				<span>+370 6904 5576</span>
