e44bcaa48ee56a365ef6eaa4f4d2584d57455f78
Gvidas Gaidauskas <gd.gvidas@gmail.com>
2022-07-10 19:43:39 +0300

    Remove typing animation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import Picture from 'components/Picture/Picture'
 import Links from 'components/Links/Links'
-import Description from 'components/Description/Description'
+import { Description } from 'components/Description/Description'
 import styled from 'styled-components'
 
 const AppWrapper = styled.article`
--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,6 +1,5 @@
 import { TransitionWrapper } from 'components/Transition/TransitionWrapper'
 import { color } from 'enums/color'
-import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.section`
@@ -37,36 +36,17 @@ const Title = styled.span`
 	min-height: 24px;
 `
 
-const FULL_TITLE = 'Software engineer'
-
-const Description = () => {
-	const [title, setTitle] = useState('')
-
-	useEffect(() => {
-		if (FULL_TITLE !== title) {
-			setTimeout(
-				() => {
-					setTitle(title + FULL_TITLE[FULL_TITLE.indexOf(title) + title.length])
-				},
-				!title.length ? 2000 : 100
-			)
-		}
-	}, [title])
-
-	return (
-		<TransitionWrapper duration={1500}>
-			<Wrapper>
-				<h2>Gvidas Gaidauskas</h2>
-				<Title>{title}</Title>
-				<Contacts>
-					<Email href="mailto:gd.gvidas@gmail.com" type="email">
-						gd.gvidas@gmail.com
-					</Email>
-					<span>+370 6904 5576</span>
-				</Contacts>
-			</Wrapper>
-		</TransitionWrapper>
-	)
-}
-
-export default Description
+export const Description = () => (
+	<TransitionWrapper duration={1500}>
+		<Wrapper>
+			<h2>Gvidas Gaidauskas</h2>
+			<Title>Software engineer</Title>
+			<Contacts>
+				<Email href="mailto:gd.gvidas@gmail.com" type="email">
+					gd.gvidas@gmail.com
+				</Email>
+				<span>+370 6904 5576</span>
+			</Contacts>
+		</Wrapper>
+	</TransitionWrapper>
+)
